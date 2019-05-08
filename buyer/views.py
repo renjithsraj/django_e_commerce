5ec9d6e44b36686a9820425da2b70da1245d9d57
renjith s raj <renjithsraj@live.com>
2019-05-08 11:01:27 +0530

    Added first template home and done login with builtin django auth

--- buyer/views.py
+++ buyer/views.py
@@ -1,3 +1,152 @@
-from django.shortcuts import render
+# import json
+# import datetime
+# import random
+# import re
+# from django.conf import settings
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from django.core.mail import EmailMultiAlternatives
+# from django.template.loader import render_to_string
+# from django.utils.html import strip_tags
+# from django.template.loader import get_template
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+# from .models import Buyer, Pincode
+# from django.utils import simplejson
 
-# Create your views here.
+# # buyer_login
+
+
+# # buyer_login
+# def login(request):
+#     req = request.POST.get
+#     if req.get('username') and req.get('password'):
+
+
+
+
+
+
+
+# @dajaxice_register
+# def buyer_login(request, username, password, next_url=None):
+
+#     dajax = Dajax()
+#     dajax.assign('#login-error', 'innerHTML', '')
+#     if username and password:
+#         print username, password
+#         print "Login"
+
+#         user = authenticate(username=username, password=password)
+#         print "vvvvvvvvvvvvvvvv", user
+
+#         if user:
+#             if not user.is_staff:
+#                 print "login................d"
+#                 login(request, user)
+#                 print "login............ssssssssss"
+#                 move_to_cart(request)
+#                 print "after move........."
+#                 if next_url:
+#                     dajax.redirect(next_url)
+#                 else:
+#                     dajax.script('LocationReload();')
+#             else:
+#                 return simplejson.dumps({'status': 'warning', 'message': 'Staff can not Login here.'})
+#         else:
+#             print "Password Error"
+#             return simplejson.dumps({'status': 'warning', 'message': 'Username & Password Not Matching'})
+#     else:
+#         print "Enter Valid Form"
+#         return simplejson.dumps({'status': 'warning', 'message': 'Enter Valid Form.'})
+
+#     return dajax.json()
+
+
+# # # buyer_login
+# # @dajaxice_register
+# # def signup(request, name=None, email=None, mobile=None, pwd=None, cpwd=None, address=None, zipcode=None, city=None, country=None, state=None):
+
+# #     dajax = Dajax()
+# #     if name and email and mobile and pwd and cpwd:
+
+# #         print name, email, mobile, pwd
+# #         mobile = int(mobile)
+# #         zipcode = int(zipcode)
+
+# #         if type(mobile) and type(zipcode) == int:
+
+# #             if User.objects.filter(email=email) or User.objects.filter(username=email):
+# #                 print "######### Existing Email ###########"
+
+# #                 return simplejson.dumps({'status': 'warning', 'message': 'A User already exist with this email address.Please select a different email address or login'})
+
+# #             else:
+# #                 if pwd != cpwd:
+# #                     return simplejson.dumps({'status': 'warning', 'message': 'password mis-match please try again..'})
+# #                 else:
+# #                     sub = Buyer()
+# #                     sub.first_name = name
+# #                     sub.set_password(pwd)
+# #                     sub.mobile = mobile
+# #                     sub.username = email
+# #                     sub.email = email
+# #                     sub.street_address = address
+# #                     sub.pincode = zipcode
+# #                     sub.city = city
+# #                     sub.country = country
+# #                     sub.state = state
+# #                     sub.key_generated = datetime.datetime.now()
+# #                     salt = sha.new(str(random.random())).hexdigest()[:5]
+# #                     sub.verification_code = sha.new(
+# #                         salt + sub.email).hexdigest()
+# #                     sub.save()
+
+# #                     print "hahahah inside just click"
+
+# #                     user = authenticate(username=sub.username, password=pwd)
+# #                     if user:
+# #                         login(request, user)
+# #                         move_to_cart(request)
+
+# #                         subject = 'Account created successfully'
+
+# #                         val = {
+# #                             'site_url': settings.SITE_URL,
+# #                             'logo': settings.COMPANY_LOGO,
+# #                             'fb': settings.COMPANY_FB_URL,
+# #                             'twitter': settings.COMPANY_TWITTER_URL,
+# #                             'gplus': settings.COMPANY_GPLUS_URL,
+# #                             'subject': subject,
+# #                             'name': name,
+# #                             'mobile': mobile,
+# #                             'email': email,
+# #                             'username': email,
+# #                             'password': pwd,
+# #                             'verification_key': sub.verification_code,
+# #                         }
+
+# #                         html_content = render_to_string(
+# #                             'mail/welcome.html', val)
+# #                         text_content = strip_tags(html_content)
+
+# #                         msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, to=[
+# #                                                      sub.email, 'settings.DEFAULT_TO_EMAIL'])
+# #                         msg.attach_alternative(html_content, "text/html")
+# #                         msg.send(fail_silently=True)
+
+# #                         print "Mail send"
+
+# #                         dajax.script(
+# #                             'Success("Your account created successfully ");')
+
+# #                         return simplejson.dumps({'status': 'success', 'message': 'Well done! Your account created successfully. Please verify your mail id.'})
+
+# #         else:
+
+# #             return simplejson.dumps({'status': 'warning', 'message': 'Pincode and Phone number Must be Integear'})
+
+# #     else:
+# #         print "Enter Valid Form"
+# #         return simplejson.dumps({'status': 'warning', 'message': 'Enter Valid Form.'})
+# #     return dajax.json()
